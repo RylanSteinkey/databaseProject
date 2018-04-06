a81afef82c0607aa736fd775218198fb080f02bd
darshthanki <darsh.thanki@wbms.ca>
2018-04-06 12:40:26 -0600

    Added header and CSS styling for navigation.
    Also updated warranty.html to use prefilled dropdowns.

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+
+    echo'
+    
+    <div class="header">
+    
+        <h1>Dealership Management System</h1>
+        
+        <ul>
+            <li><a href="/new_customer.html">New Customer</a></li>
+            <li><a href="/new_dealer.html">New Dealer</a></li>
+            <li><a href="/purchase_form.html">Dealership Purchase</a></li>
+            <li><a href="/car_sale.html">Car Sale</a></li>
+            <li><a href="/warranty.html">Warranty</a></li>
+        </ul>
+    
+    </div>
+    '
+
+?>
