a2eb5da7718093c1cc4ddb3d564a9ac58a98056e
darshthanki <darsh.thanki@wbms.ca>
2018-04-07 15:28:10 -0600

    Updated navigation. Added remaining columns in all customers report.

--- header.php
+++ header.php
@@ -5,12 +5,12 @@
     <div class="header">
     
         <img src="./css/img/graphicDMS.png" alt="logo" width="300px"/>
-        
+        <br/>
         <ul>
             <li><a href="/new_customer.html">New Customer</a></li>
             <li><a href="/new_dealer.html">New Dealer</a></li>
             <li><a href="/purchase_form.html">Dealership Purchase</a></li>
-            <li><a href="/car_sale.html">Car Sale</a></li>
+            <li><a href="/car_sale.php">Car Sale</a></li>
             <li><a href="/warranty.html">Warranty</a></li>
         </ul>
     
