55d9aa4420b004af7ddf641ecb0f227ffd48c706
Isaac Ekra <cslimwright@gmail.com>
2018-04-06 22:37:51 -0600

    Update customer.php

--- customer.php
+++ customer.php
@@ -7,69 +7,62 @@
 
   if (isset($_POST['customer_submit']))
   {
-    $customer_id = "C-000000001";
-    $employment_history_id = "H-000000001";
-    $num_late_payment = 0;
+    $num_late_payments = 0;
     $avg_days_late= 0;
-    $first_name = $last_name = $email = $phone = $city = $street_name = "";
-    $postal_code = $province = $gender = $tax_id = $notes = "";
 
-    $first_name = mysqli_real_escape_string($_POST ['first_name']);
-    $last_name = mysqli_real_escape_string($_POST ['last_name']);
-    $email = mysqli_real_escape_string($_POST ['email']);
-    $phone = mysqli_real_escape_string($_POST ['phone']);
-    $city = mysqli_real_escape_string($_POST ['city']);
-    $street_name = mysqli_real_escape_string($_POST ['street_name']);
-    $postal_code = mysqli_real_escape_string($_POST ['postal_code']);
-    $province = mysqli_real_escape_string($_POST ['province']);
-    $gender = mysqli_real_escape_string($_POST ['gender']);
-    $tax_id = mysqli_real_escape_string($_POST ['tax_id']);
-    $notes = mysqli_real_escape_string($_POST ['notes']);
+    $first_name = mysqli_real_escape_string($conn, $_POST['first_name']);
+    $last_name = mysqli_real_escape_string($conn, $_POST['last_name']);
+    $dob = mysqli_real_escape_string($conn, $_POST['DOB']);
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
+    $phone = mysqli_real_escape_string($conn, $_POST['phone']);
+    $city = mysqli_real_escape_string($conn, $_POST['city']);
+    $street_name = mysqli_real_escape_string($conn, $_POST['street_name']);
+    $pcode = mysqli_real_escape_string($conn, $_POST['postal_code']);
+    $province = mysqli_real_escape_string($conn, $_POST['province']);
+    $gender = mysqli_real_escape_string($conn, $_POST['gender']);
+    $tax_id = mysqli_real_escape_string($conn, $_POST['tax_id']);
+    $notes = mysqli_real_escape_string($conn, $_POST['notes']);
 
-    $employer = $title = $supervisor = $employer_phone = $employer_city = $employer_street = "";
-    $employer_postal = $employer_province = $date_start = $date_end = "";
-
-    $employer = mysqli_real_escape_string($_POST ['employer']);
-    $title = mysqli_real_escape_string($_POST ['title']);
-    $supervisor = mysqli_real_escape_string($_POST ['supervisor']);
-    $employer_phone = mysqli_real_escape_string($_POST ['employer_phone']);
-    $employer_city = mysqli_real_escape_string($_POST ['employer_city']);
-    $employer_street = mysqli_real_escape_string($_POST ['employer_street']);
-    $employer_postal = mysqli_real_escape_string($_POST ['employer_postal']);
-    $employer_province = mysqli_real_escape_string($_POST ['employer_province']);
-    $date_start = mysqli_real_escape_string($_POST ['start_date']);
-    $date_end = mysqli_real_escape_string($_POST ['end_date']);
-
-    $sql2 = "INSERT INTO customer_employment_history (customer_id, employment_history_id, employer, title,
-    supervisor, phone, city, street_name, postal_code, province, start_date, end_date)
-    VALUES('$customer_id', '$employment_history_id', '$employer', '$title', '$supervisor', '$employer_phone',
-    '$employer_city', '$employer_street', '$employer_postal', '$employer_province' ,'$date_start', '$date_end')";
-
-    if ($conn->query($sql2) === TRUE){
-      $sql = "INSERT INTO customer (customer_id, first_name , last_name , email , phone , city , street_name,
-      postal_code, province ,gender ,tax_id ,num_late_payments ,avg_days_late,
-      notes ,employment_history_id)
-      VALUES ('$customer_id', '$first_name', '$last_name', '$email', '$phone', '$city', '$street_name',
-      '$postal_code', '$province' , '$gender' ,'$tax_id', '$num_late_payment', '$avg_days_late',
-      '$notes', '$employment_history_id')";
+    $employer = mysqli_real_escape_string($conn, $_POST['employer']);
+    $title = mysqli_real_escape_string($conn, $_POST['title']);
+    $supervisor = mysqli_real_escape_string($conn, $_POST['supervisor']);
+    $employer_phone = mysqli_real_escape_string($conn, $_POST['employer_phone']);
+    $employer_city = mysqli_real_escape_string($conn, $_POST['employer_city']);
+    $employer_street = mysqli_real_escape_string($conn, $_POST['employer_street']);
+    $employer_postal = mysqli_real_escape_string($conn, $_POST['employer_postal']);
+    $employer_province = mysqli_real_escape_string($conn, $_POST['employer_province']);
+    $date_start = mysqli_real_escape_string($conn, $_POST['start_date']);
+    $date_end = mysqli_real_escape_string($conn, $_POST['end_date']);
 
 
-      	if ($conn->query($sql) === TRUE){
-          header('Location: ../new_customer');
-          exit;
-        } else {
-          echo "Error: " . $sql . "<br>" . $conn->error;
-          die();
-        }
 
+    $sql = "INSERT INTO customer (first_name , last_name, gender, DOB, phone, email, tax_id , num_late_payments,
+    avg_days_late, street_name ,city ,province ,postal_code ,notes)
+    VALUES ('$first_name', '$last_name', '$gender', '$dob', '$phone', '$email',
+    '$tax_id', '$num_late_payments' , '$avg_days_late' ,'$street_name', '$city', '$province', '$pcode', '$notes')";
+
+    if ($conn->query($sql) === TRUE)
+    {
+      $customer_id = $conn->insert_id;
+
+      $sql2 = "INSERT INTO customer_employment_history (customer_id, employer, title,
+      supervisor, start_date, end_date, phone, city, street_name, postal_code, province)
+      VALUES('$customer_id', '$employer', '$title', '$supervisor', '$date_start', '$date_end', '$employer_phone',
+      '$employer_city', '$employer_street', '$employer_postal', '$employer_province')";
+      if ($conn->query($sql2) === TRUE)
+      {
+        header('Location: ../new_customer');
+        exit;
+      } else
+      {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+        die();
       }
-
-    else {
-      echo "Error: " . $sql2 . "<br>" . $conn->error;
+    } else
+    {
+      echo "Error: " . $sql . "<br>" . $conn->error;
       die();
     }
-
-
 
     $conn->close();
   }
