daa1030a43aac652c7d1edd92c4e378613138b00
Isaac Ekra <cslimwright@gmail.com>
2018-04-07 02:18:44 -0600

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,27 +1,30 @@
--- MySQL dump 10.13  Distrib 5.7.21, for osx10.13 (x86_64)
---
--- Host: localhost    Database: dms_db
--- ------------------------------------------------------
--- Server version	5.7.21
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.21)
+# Database: dms_db
+# Generation Time: 2018-04-07 08:17:47 +0000
+# ************************************************************
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Table structure for table `car_purchase`
---
+
+# Dump of table car_purchase
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `car_purchase`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `car_purchase` (
   `purch_order_no` int(11) NOT NULL AUTO_INCREMENT,
   `dealer_id` int(11) NOT NULL,
@@ -38,24 +41,14 @@ CREATE TABLE `car_purchase` (
   CONSTRAINT `car_purchase_ibfk_1` FOREIGN KEY (`vehicle_id`) REFERENCES `vehicle` (`vehicle_id`),
   CONSTRAINT `car_purchase_ibfk_2` FOREIGN KEY (`dealer_id`) REFERENCES `dealer` (`dealer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `car_purchase`
---
 
-LOCK TABLES `car_purchase` WRITE;
-/*!40000 ALTER TABLE `car_purchase` DISABLE KEYS */;
-/*!40000 ALTER TABLE `car_purchase` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `car_sale`
---
+# Dump of table car_sale
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `car_sale`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `car_sale` (
   `sale_id` int(11) NOT NULL AUTO_INCREMENT,
   `customer_id` int(11) NOT NULL,
@@ -77,24 +70,14 @@ CREATE TABLE `car_sale` (
   CONSTRAINT `car_sale_ibfk_2` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`emp_id`),
   CONSTRAINT `car_sale_ibfk_3` FOREIGN KEY (`vehicle_id`) REFERENCES `vehicle` (`vehicle_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `car_sale`
---
 
-LOCK TABLES `car_sale` WRITE;
-/*!40000 ALTER TABLE `car_sale` DISABLE KEYS */;
-/*!40000 ALTER TABLE `car_sale` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `customer`
---
+# Dump of table customer
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `customer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `customer` (
   `customer_id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(50) NOT NULL,
@@ -110,27 +93,28 @@ CREATE TABLE `customer` (
   `city` varchar(50) NOT NULL,
   `province` varchar(50) NOT NULL,
   `postal_code` varchar(20) NOT NULL,
+  `notes` text,
   `address` varchar(170) GENERATED ALWAYS AS (concat_ws(' ',`street_name`,`city`,`province`,`postal_code`)) VIRTUAL,
   PRIMARY KEY (`customer_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `customer`
---
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 LOCK TABLES `customer` WRITE;
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
+
+INSERT INTO `customer` (`customer_id`, `first_name`, `last_name`, `gender`, `DOB`, `phone`, `email`, `tax_id`, `num_late_payments`, `avg_days_late`, `street_name`, `city`, `province`, `postal_code`, `notes`, `address`)
+VALUES
+	(4,'Bender','Rodriguez','0','2017-11-30','(780)862-4012','a@b.c','123456789',0,0,'1 Ave St','Calgary','Ontario','T1K8A2','','1 Ave St Calgary Ontario T1K8A2'),
+	(5,'Yao','Hoa','1','2018-04-01','(123)512-2301','a@b.c','123091234',0,0,'123 Fake St','Aurora','Ontario','M3A2Z9','','123 Fake St Aurora Ontario M3A2Z9');
+
 /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `customer_employment_history`
---
+
+# Dump of table customer_employment_history
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `customer_employment_history`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `customer_employment_history` (
   `employment_history_id` int(11) NOT NULL AUTO_INCREMENT,
   `customer_id` int(11) NOT NULL,
@@ -148,25 +132,25 @@ CREATE TABLE `customer_employment_history` (
   PRIMARY KEY (`employment_history_id`),
   KEY `customer_id` (`customer_id`),
   CONSTRAINT `customer_employment_history_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `customer_employment_history`
---
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 LOCK TABLES `customer_employment_history` WRITE;
 /*!40000 ALTER TABLE `customer_employment_history` DISABLE KEYS */;
+
+INSERT INTO `customer_employment_history` (`employment_history_id`, `customer_id`, `employer`, `title`, `supervisor`, `start_date`, `end_date`, `phone`, `street_name`, `city`, `province`, `postal_code`, `address`)
+VALUES
+	(2,4,'Best','empoo','mcgee','2017-07-29','2015-11-30','(123)456-7890','2 Ave S','Lethbridge','Alberta','T1K4T6','2 Ave S Lethbridge Alberta T1K4T6'),
+	(3,5,'Harliett','Manager','Gumbee','2018-04-01','2018-04-10','1239024012','1 ft st','Lethbridge','Alberta','T1K8A2','1 ft st Lethbridge Alberta T1K8A2');
+
 /*!40000 ALTER TABLE `customer_employment_history` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `dealer`
---
+
+# Dump of table dealer
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `dealer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `dealer` (
   `dealer_id` int(11) NOT NULL AUTO_INCREMENT,
   `company_name` varchar(50) NOT NULL,
@@ -182,25 +166,15 @@ CREATE TABLE `dealer` (
   `postal_code` varchar(20) NOT NULL,
   `address` varchar(170) GENERATED ALWAYS AS (concat_ws(' ',`street_name`,`city`,`province`,`postal_code`)) VIRTUAL,
   PRIMARY KEY (`dealer_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `dealer`
---
 
-LOCK TABLES `dealer` WRITE;
-/*!40000 ALTER TABLE `dealer` DISABLE KEYS */;
-/*!40000 ALTER TABLE `dealer` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `employee`
---
+# Dump of table employee
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `employee`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `employee` (
   `emp_id` int(11) NOT NULL AUTO_INCREMENT,
   `employee_type` varchar(25) NOT NULL,
@@ -213,25 +187,24 @@ CREATE TABLE `employee` (
   `postal_code` varchar(20) NOT NULL,
   `address` varchar(170) GENERATED ALWAYS AS (concat_ws(' ',`street_name`,`city`,`province`,`postal_code`)) VIRTUAL,
   PRIMARY KEY (`emp_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `employee`
---
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 LOCK TABLES `employee` WRITE;
 /*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+
+INSERT INTO `employee` (`emp_id`, `employee_type`, `first_name`, `last_name`, `phone`, `street_name`, `city`, `province`, `postal_code`, `address`)
+VALUES
+	(2,'Manager','Bender','Rodriguez','(780)872-4012','4 Ave S','Lethbridge','Alberta','T1K8A2','4 Ave S Lethbridge Alberta T1K8A2');
+
 /*!40000 ALTER TABLE `employee` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Table structure for table `payments`
---
+
+# Dump of table payments
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `payments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `payments` (
   `payment_id` int(11) NOT NULL AUTO_INCREMENT,
   `customer_id` int(11) NOT NULL,
@@ -245,24 +218,14 @@ CREATE TABLE `payments` (
   KEY `customer_id` (`customer_id`),
   CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `payments`
---
 
-LOCK TABLES `payments` WRITE;
-/*!40000 ALTER TABLE `payments` DISABLE KEYS */;
-/*!40000 ALTER TABLE `payments` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `sold_warranty`
---
+# Dump of table sold_warranty
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `sold_warranty`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `sold_warranty` (
   `sold_warranty_id` int(11) NOT NULL AUTO_INCREMENT,
   `emp_id` int(11) NOT NULL,
@@ -276,61 +239,41 @@ CREATE TABLE `sold_warranty` (
   CONSTRAINT `sold_warranty_ibfk_1` FOREIGN KEY (`emp_id`) REFERENCES `employee` (`emp_id`),
   CONSTRAINT `sold_warranty_ibfk_2` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `sold_warranty`
---
 
-LOCK TABLES `sold_warranty` WRITE;
-/*!40000 ALTER TABLE `sold_warranty` DISABLE KEYS */;
-/*!40000 ALTER TABLE `sold_warranty` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `vehicle`
---
+# Dump of table vehicle
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `vehicle`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `vehicle` (
   `vehicle_id` int(11) NOT NULL AUTO_INCREMENT,
   `customer_id` int(11) NOT NULL,
   `make` varchar(50) NOT NULL,
   `model` varchar(50) NOT NULL,
-  `v_year` date NOT NULL,
+  `v_year` int(11) NOT NULL,
   `color` varchar(25) NOT NULL,
   `style` varchar(25) NOT NULL,
   `interior_color` varchar(25) NOT NULL,
   `miles` int(5) NOT NULL,
-  `v_condition` varchar(15) NOT NULL,
+  `v_condition` varchar(50) NOT NULL DEFAULT '',
   `repair_cost` float NOT NULL,
   `book_price` float NOT NULL,
   `sale_price` float NOT NULL,
   `issue_log` text,
   PRIMARY KEY (`vehicle_id`),
   KEY `customer_id` (`customer_id`),
-  CONSTRAINT `vehicle_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  CONSTRAINT `vehicle_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `vehicle`
---
 
-LOCK TABLES `vehicle` WRITE;
-/*!40000 ALTER TABLE `vehicle` DISABLE KEYS */;
-/*!40000 ALTER TABLE `vehicle` ENABLE KEYS */;
-UNLOCK TABLES;
 
---
--- Table structure for table `warranty_type`
---
+# Dump of table warranty_type
+# ------------------------------------------------------------
 
 DROP TABLE IF EXISTS `warranty_type`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+
 CREATE TABLE `warranty_type` (
   `warranty_code` int(11) NOT NULL AUTO_INCREMENT,
   `warranty_name` varchar(50) NOT NULL,
@@ -342,24 +285,13 @@ CREATE TABLE `warranty_type` (
   `covered_items` text NOT NULL,
   PRIMARY KEY (`warranty_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `warranty_type`
---
 
-LOCK TABLES `warranty_type` WRITE;
-/*!40000 ALTER TABLE `warranty_type` DISABLE KEYS */;
-/*!40000 ALTER TABLE `warranty_type` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-04-06 17:49:27
