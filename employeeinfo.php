31aa18a95dc9fe47f29b00e6af4f435ea4191709
Isaac Ekra <cslimwright@gmail.com>
2018-04-07 01:29:37 -0600

    Update employeeinfo.php

--- employeeinfo.php
+++ employeeinfo.php
@@ -10,7 +10,7 @@ $sql = "SELECT first_name, last_name FROM employee WHERE emp_id = '".$q."'";
 $results = $conn->query($sql);
 if ($results->num_rows > 0){
 	while ($row = $results->fetch_assoc()){
-    echo "First Name: " . $row['first_name'] . " Last Name: " . $row['last_name'] . " Commission Percentage (%): <input type='number' name=\"rep_commission\">;
+    echo "First Name: " . $row['first_name'] . " Last Name: " . $row['last_name'] . " Commission Percentage (%): <input type='number' name=\"rep_commission\">";
   }
 } else {
 echo $conn->error;
