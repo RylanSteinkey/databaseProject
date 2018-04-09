1ae2b735bec1e41153eee7cbee1fbecda77d540a
darshthanki <darsh.thanki@wbms.ca>
2018-04-09 15:49:50 -0600

    Headers.

--- header.php
+++ header.php
@@ -13,15 +13,16 @@
             <li><a href="/warranty.html">Warranty Sale</a></li>
             <li><a href="/purchase_form.html">Auction Purchase</a></li>
             <li><a href="/payment.html">Customer Payment</a></li>
+            <li><a href="/new_customer.html">New Customer</a></li>
+            <li><a href="/new_dealer.html">New Dealer</a></li>
             <li><a href="/report-all-customers.html">Customer Search</a></li>
             <li><a href="/report-customer-and-vehicles.html">Vehicle Search</a></li>
             <li><a href="/report-unsold-vehicles.html">Unsold Vehicles</a></li>
             <li><a href="/report-customer_email.html">Customer Emails</a></li>
             <li><a href="/report-salesperson-sales.html">Salesperson Report</a></li>
-            <li><a href="/report-warranties-sold.html">Warraties Sold</a></li>
+            <li><a href="/report-warranties-sold.html">Warranties Sold</a></li>
             <li><a href="/report-warranty_types.html">Warranty Available</a></li>
-            <li><a href="/new_customer.html">New Customer</a></li>
-            <li><a href="/new_dealer.html">New Dealer</a></li>
+
         </ul>
     
     </div>
