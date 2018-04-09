9bafd5a285fc795da9c0333d37bf89658f74ca83
darshthanki <darsh.thanki@wbms.ca>
2018-04-08 23:49:11 -0600

    Edit stylesheet, added report warranty types

--- header.php
+++ header.php
@@ -6,12 +6,18 @@
     
         <img src="./css/img/graphicDMS.png" alt="logo" width="300px"/>
         <br/>
+        &nbsp
         <ul>
             <li><a href="/new_customer.html">New Customer</a></li>
             <li><a href="/new_dealer.html">New Dealer</a></li>
             <li><a href="/purchase_form.html">Dealership Purchase</a></li>
             <li><a href="/car_sale.html">Car Sale</a></li>
             <li><a href="/warranty.html">Warranty</a></li>
+            <li><a href="/report-all-customers.html">All Customers</a></li>
+            <li><a href="/report-customer-and-vehicles.html">Customer owned vehicles</a></li>
+            <li><a href="/report-customer_email.html">Customer Emails</a></li>
+            <li><a href="/report-salesperson-sales.html">Salesperson Total Sales</a></li>
+            <li><a href="/report-warranty_types.html">Warranty Types</a></li>
         </ul>
     
     </div>
