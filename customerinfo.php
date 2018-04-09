30add4548c1e019e863ff165d560cd611364ba04
Isaac Ekra <cslimwright@gmail.com>
2018-04-08 23:31:57 -0600

    Update customerinfo.php
    fixed formatting for generated info

--- customerinfo.php
+++ customerinfo.php
@@ -11,8 +11,12 @@ $sql = "SELECT * FROM customer WHERE customer_id = '".$q."'";
 $results = $conn->query($sql);
 if ($results->num_rows > 0){
 	while ($row = $results->fetch_assoc()){
-    echo "Customer ID: " . $row['customer_id'] . "Name: " . $row['first_name'] . " " . $row['last_name']. "Phone: " . $row['phone'] . "Email: " . $row['email']
-    . "<br> Address: " . $row['address'];
+		echo "Customer ID: <input type='text' name='customer_id' value='" . $row['customer_id'] . "' readonly> ";
+		echo "First Name: <input type='text' name='first_name' value='" . $row['first_name'] . "' readonly> ";
+		echo "Last Name: <input type='text' name='last_name' value='" . $row['last_name'] . "' readonly> <br>";
+		echo "Phone Number: <input type='text' name='phone' value='" . $row['phone'] . "' readonly> ";
+		echo "Email: <input type='text' name='email' value='" . $row['email'] . "' readonly> ";
+		echo "Address: <input type='text' name='address' value='" . $row['address'] . "' readonly> ";
   }
 } else {
 echo $conn->error;
