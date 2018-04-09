f64355cb6e15f3501d737886ba42f2a6116fff9f
darshthanki <darsh.thanki@wbms.ca>
2018-04-09 14:39:10 -0600

    Updated header

--- header.php
+++ header.php
@@ -8,16 +8,19 @@
         <br/>
         &nbsp
         <ul>
-            <li><a href="/new_customer.html">New Customer</a></li>
-            <li><a href="/new_dealer.html">New Dealer</a></li>
-            <li><a href="/purchase_form.html">Dealership Purchase</a></li>
-            <li><a href="/car_sale.html">Car Sale</a></li>
-            <li><a href="/warranty.html">Warranty</a></li>
-            <li><a href="/report-all-customers.html">All Customers</a></li>
-            <li><a href="/report-customer-and-vehicles.html">Customer owned vehicles</a></li>
+            <li><a href="/index.html">Home</a></li>
+            <li><a href="/car_sale.html">Vehicle Sale</a></li>
+            <li><a href="/warranty.html">Warranty Sale</a></li>
+            <li><a href="/purchase_form.html">Auction Purchase</a></li>
+            <li><a href="/payment.html">Customer Payment</a></li>
+            <li><a href="/report-all-customers.html">Customer Search</a></li>
+            <li><a href="/report-customer-and-vehicles.html">Vehicle Search</a></li>
+            <li><a href="/report-unsold-vehicles.html">Unsold Vehicles</a></li>
             <li><a href="/report-customer_email.html">Customer Emails</a></li>
             <li><a href="/report-salesperson-sales.html">Salesperson Total Sales</a></li>
             <li><a href="/report-warranty_types.html">Warranty Types</a></li>
+            <li><a href="/new_customer.html">New Customer</a></li>
+            <li><a href="/new_dealer.html">New Dealer</a></li>
         </ul>
     
     </div>
