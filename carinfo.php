b00231cd71b862279e1f3e7031faede685c45c15
Isaac Ekra <cslimwright@gmail.com>
2018-04-08 23:31:27 -0600

    Update carinfo.php
    fixed formatting for generated info

--- carinfo.php
+++ carinfo.php
@@ -11,8 +11,12 @@ $sql = "SELECT * FROM vehicle WHERE vehicle_id = '".$q."'";
 $results = $conn->query($sql);
 if ($results->num_rows > 0){
 	while ($row = $results->fetch_assoc()){
-    echo "Vehicle Make: " . $row['make'] . "Vehicle Model " . $row['model'] . "Vehicle Year: " . $row['v_year'] . "Vehicle Color: " . $row['color']
-    . "<br> Vehicle Style: " . $row['style'] . "Interior Color: " . $row['interior_color'];
+		echo "Vehicle Make: <input type='text' name='make' value='" . $row['make'] . "' readonly> ";
+		echo "Vehicle Model: <input type='text' name='model' value='" . $row['model'] . "' readonly> ";
+		echo "Vehicle Year: <input type='text' name='v_year' value='" . $row['v_year'] . "' readonly> <br>";
+		echo "Vehicle Color: <input type='text' name='color' value='" . $row['color'] . "' readonly>";
+		echo "Vehicle Style: <input type='text' name='style' value='" . $row['style'] . "' readonly> ";
+		echo "Interior Color: <input type='text' name='interior_color' value='" . $row['interior_color'] . "' readonly> ";
   }
 } else {
 echo $conn->error;
