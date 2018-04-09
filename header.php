0513da5151c26db735d84409ae1c8afa1e700de0
darshthanki <darsh.thanki@wbms.ca>
2018-04-08 22:47:38 -0600

    Updated filetypes from PHP to HTML. Created customer email report

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
             <li><a href="/new_customer.html">New Customer</a></li>
             <li><a href="/new_dealer.html">New Dealer</a></li>
             <li><a href="/purchase_form.html">Dealership Purchase</a></li>
-            <li><a href="/car_sale.php">Car Sale</a></li>
+            <li><a href="/car_sale.html">Car Sale</a></li>
             <li><a href="/warranty.html">Warranty</a></li>
         </ul>
     
